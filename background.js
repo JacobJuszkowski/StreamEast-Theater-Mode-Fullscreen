f3f9bb71c2d33ddac22cc4064395f387343c081c
JacobJuszkowski <jjuszkowski@outlook.com>
2024-02-12 21:29:29 -0500

    Remove unneeded code

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    console.log('Test');
     await chrome.scripting.executeScript({
       target: {tabId: tab.id},
       files: ['inject.js']
